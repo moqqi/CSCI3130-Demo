937f805d126f77bdb455948a32bd861381cc788f
iMoqii <rnewson@dal.ca>
2018-02-07 16:10:51 -0400

    Added validation cases to Validator Class
    Added a check for password requiring digit, max length of 16 chars, and at least one upper case value.

--- app/src/main/java/com/example/acme/csci3130_demo/Validator.java
+++ app/src/main/java/com/example/acme/csci3130_demo/Validator.java
@@ -7,16 +7,35 @@ package com.example.acme.csci3130_demo;
 public class Validator {
     public int validate(String password){
         return validateCase(password)       +
-                validateMinLength(password) ;
+                validateMinLength(password) +
+                validateMaxLength(password) +
+                validateDigit(password)     +
+                validateUpperCase(password);
     }
 
     private int validateCase (String password){
-        if(!password.contentEquals("password")) return 1;
+        if(!password.contentEquals( "password")) return 1;
         else return 0;
     }
 
     private int validateMinLength (String password){
-        if(password.length() >= 8) return 1;
+        if(password.length() >= 7) return 1;
+        else return 0;
+    }
+
+    private int validateMaxLength (String password){
+        if(password.length() < 16) return 1;
+        else return 0;
+    }
+
+    private int validateDigit (String password){
+        if(password.matches(".*\\d+.*")) return 1;
+        else return 0;
+    }
+
+    private int validateUpperCase (String password){
+        String lowerCasePassword = password.toLowerCase();
+        if(!password.contentEquals(lowerCasePassword)) return 1;
         else return 0;
     }
 }
